5119ac3dddc54559cedb3dd3c1cf43980f437756
canthony2 <canthony2@L11714042777.wctc.msft>
2017-04-24 14:49:01 -0500

    More practice

--- src/practice/demo1.java
+++ src/practice/demo1.java
@@ -5,10 +5,12 @@
  */
 package practice;
 
+import common.Employee;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -18,22 +20,28 @@ public class demo1 {
     public static void main(String[] args) {
         
         //HashMap, Hashtable, TreeMap, LinkedHashMap
+        //(key, value)
         
-//        Map myMap = new HashMap();
-//        myMap.put("small", new Integer(5));
-//        myMap.put("medium", new Integer(100));
-//        myMap.put("large", new Integer(1000));
-//        
-//        Integer i = (Integer)myMap.get("medium");
-//        System.out.println(i);
-
+        //No Generics
         Map myMap = new HashMap();
-        myMap.put(1, "January");
-        myMap.put(2, "February");
-        myMap.put(3, "March");
+        myMap.put("small", new Integer(5));
+        myMap.put("medium", new Integer(100));
+        myMap.put("large", new Integer(1000));
         
-        String m = (String)myMap.get(1);
+        Integer i = (Integer)myMap.get("medium");
+        System.out.println(i);
+
+        //No Generics
+        Map monthMap = new HashMap();
+        monthMap.put(1, "January");
+        monthMap.put(2, "February");
+        monthMap.put(3, "March");
+        
+        String m = (String)monthMap.get(1);
         System.out.println(m);
+        
+        //With Generics
+        //Map<key, value> mapName = new MapType<>();
         
     }
 }
