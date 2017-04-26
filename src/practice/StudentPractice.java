a151f9d2ebe67882c590a1ca96da3494a9180085
canthony2 <canthony2@L11714042777.wctc.msft>
2017-04-26 14:02:25 -0500

    Fixed keys

--- src/practice/StudentPractice.java
+++ src/practice/StudentPractice.java
@@ -16,8 +16,8 @@ public class StudentPractice {
         Movie m1 = new Movie("2", "Rogue One: A Star Wars Story", "Gareth Edwards");
         Movie m2 = new Movie("4", "Pacific Rim", "Guillermo del Toro");
         Movie m3 = new Movie("1", "Battleship", "Peter Berg");
-        Movie m4 = new Movie("2", "Rogue One: A Star Wars Story", "Gareth Edwards");
-        Movie m5 = new Movie("1", "Battleship", "Peter Berg");
+        Movie m4 = new Movie("3", "Rogue One: A Star Wars Story", "Gareth Edwards");
+        Movie m5 = new Movie("10", "Battleship", "Peter Berg");
         Movie m6 = new Movie("5", "Star Wars: A New Hope", "George Lucas");
         Movie m7 = new Movie("9", "Star Trek: The Motion Picture", "Robert Wise");
         Movie m8 = new Movie("6", "Spaceballs", "Mel Brooks");
