a0f418c0814afc597236f5d7b9b5f98363157344
knyghtspup <knyghtspup@BrokenWindows>
2017-04-26 10:39:49 -0500

    Made HashMap and added movies

--- src/practice/StudentPractice.java
+++ src/practice/StudentPractice.java
@@ -25,7 +25,19 @@ public class StudentPractice {
         
         //create map to hold movies
         Map movieMap = new HashMap();
+        movieMap.put("2", m1);
+        movieMap.put("4", m2);
+        movieMap.put("1", m3);
+        movieMap.put("3", m4);
+        movieMap.put("10", m5);
+        movieMap.put("5", m6);
+        movieMap.put("9", m7);
+        movieMap.put("6", m8);
+        movieMap.put("8", m9);
+        movieMap.put("7", m10);
         
+        Movie m = (Movie)movieMap.get("9");
+        System.out.println("The movie information with the ID of: 9 is " + m);
         
     }
 }
